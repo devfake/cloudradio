52f8b2d40ad88e782f726ea5150449d56ea878a5
devfake <devfakeplus@googlemail.com>
2016-06-20 14:57:45 +0200

    e2e for track sharing

--- client/tests/e2e/specs/share.js
+++ client/tests/e2e/specs/share.js
@@ -1,3 +1,13 @@
 module.exports = {
-  '@disabled': true
+  '@disabled': false,
+
+  'Play shared track': function(client) {
+    client
+    .url(client.launch_url + '184757886')
+    .pause(2000)
+    .assert.containsText('.title', 'Alan Walker - Spectre [NCS Release]')
+    .assert.containsText('.author', 'NCS')
+    .assert.containsText('.genre', 'Electro House')
+    .end();
+  }
 };
